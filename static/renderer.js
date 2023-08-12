636259ff8aa23e390626f2ec5e677f4405ca7d86
William Sandvej Hansen <grusinator@gmail.com>
2023-08-12 13:19:35 +0200

    better, rendering the geoms but wrong

--- static/renderer.js
+++ static/renderer.js
@@ -1,39 +1,53 @@
 // renderer.js
+
 let scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff); // Set the background color to white
+scene.background = new THREE.Color(0xffffff);  // Set the background color to white
+
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-fetch('/box')
+fetch('/collection')  // Assuming this endpoint returns the list of boxes
     .then(response => response.json())
-    .then(data => {
-        // Create the box geometry from the data received from FastAPI
-        let geometry = new THREE.BoxGeometry(data.width, data.height, data.depth);
+    .then(boxes => {
+        boxes.forEach(data => {
+            let width = data.end.x - data.start.x;
+            let height = data.end.y - data.start.y;
+            let depth = data.end.z - data.start.z;
 
-        // Surface material: wood-like brown
-        let material = new THREE.MeshBasicMaterial({color: 0x8B4513});
-        let cube = new THREE.Mesh(geometry, material);
+            let geometry = new THREE.BoxGeometry(width, height, depth);
 
-        // Edge material: black
-        let edges = new THREE.EdgesGeometry(geometry);
-        let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x000000}));
+            // Surface material: wood-like brown
+            let material = new THREE.MeshBasicMaterial({color: 0x8B4513});
+            let cube = new THREE.Mesh(geometry, material);
 
-        // Add both cube and line to a group so they rotate together
-        let group = new THREE.Group();
-        group.add(cube);
-        group.add(line);
+            // Edge material: black
+            let edges = new THREE.EdgesGeometry(geometry);
+            let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x000000}));
 
-        scene.add(group);
+            // Adjusting cube's position based on the start coordinates
+            cube.position.set(data.start.x + width/2, data.start.y + height/2, data.start.z + depth/2);
 
-        camera.position.z = 5;
+            // Add both cube and line to a group so they rotate together
+            let group = new THREE.Group();
+            group.add(cube);
+            group.add(line);
+
+            scene.add(group);
+        });
+
+        // Set the camera position dynamically based on the size of the box.
+        // This is an approximation to make sure the camera is far enough from the objects to view them properly.
+        let maxDimension = Math.max(boxes[0].end.x - boxes[0].start.x, boxes[0].end.y - boxes[0].start.y, boxes[0].end.z - boxes[0].start.z);
+        camera.position.z = maxDimension * 3;
 
         // Render loop with added rotation
         function animate() {
             requestAnimationFrame(animate);
-            group.rotation.x += 0.005;
-            group.rotation.y += 0.005;
+            scene.rotation.x += 0.005;
+            scene.rotation.y += 0.005;
             renderer.render(scene, camera);
         }
 
